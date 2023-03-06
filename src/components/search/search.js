98309f504c105398e86ea81166bc2d3244106bd0
Iman Kalyan Majumder <77956318+Iman-Kalyan-Majumder@users.noreply.github.com>
2023-03-06 11:13:45 +0530

    Reduced the Population Limit to 100,000

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,31 +1,31 @@
-import {AsyncPaginate} from "react-select-async-paginate";
+import { AsyncPaginate } from "react-select-async-paginate";
 import { useState } from "react";
 import { geoApiOptions } from "../../api";
 import { GEO_API_URL } from "../../api";
 
-const Search =({onSearchChange})=>{
+const Search = ({ onSearchChange }) => {
 
-    const [search,setSearch]=useState(null);
+    const [search, setSearch] = useState(null);
 
-    const handleOnChange=searchData=>{
+    const handleOnChange = searchData => {
         setSearch(searchData);
         onSearchChange(searchData);
     }
 
-    const loadOptions=inputValue=>{
-        return fetch(`${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`, geoApiOptions)
-        .then(response => response.json())
-        .then(response =>{
-            return {
-                options:response.data.map((city)=>{
-                    return {
-                        value: `${city.latitude} ${city.longitude}`,
-                        label: `${city.name}, ${city.countryCode}`,
-                    }
-                })
-            }
-        })
-        .catch(err => console.error(err));
+    const loadOptions = inputValue => {
+        return fetch(`${GEO_API_URL}/cities?minPopulation=100000&namePrefix=${inputValue}`, geoApiOptions)
+            .then(response => response.json())
+            .then(response => {
+                return {
+                    options: response.data.map((city) => {
+                        return {
+                            value: `${city.latitude} ${city.longitude}`,
+                            label: `${city.name}, ${city.countryCode}`,
+                        }
+                    })
+                }
+            })
+            .catch(err => console.error(err));
     }
 
     return (
